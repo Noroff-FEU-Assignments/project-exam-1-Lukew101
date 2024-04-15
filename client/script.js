079d8114dcc2233d6fc0d330a44dcaa587840a42
Luke Williams <lukewilliams10101@gmail.com>
2024-04-15 21:50:57 +0200

    feat(): burger menu triggers menu to slide in

--- client/script.js
+++ client/script.js
@@ -1,1 +1,16 @@
+const burger = document.querySelector(".burger-menu");
+const nav = document.querySelector(".header-nav");
 
+// Nav menu
+const navSlide = () => {
+  burger.addEventListener("click", () => {
+    if (nav.classList.contains("nav-active")) {
+      nav.classList.remove("nav-active");
+      burger.classList.remove("toggle");
+    } else {
+      nav.classList.add("nav-active");
+      burger.classList.add("toggle");
+    }
+  });
+};
+navSlide();
