9730bbf950c1f47540e5da824040bf3dd2855f6d
Luke Williams <lukewilliams10101@gmail.com>
2024-04-29 22:45:33 +0200

    feat(): form functioning

--- client/contact/contact.js
+++ client/contact/contact.js
@@ -1,13 +1,18 @@
+const submitMessage = document.querySelector(".submit-message");
+
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contact-form");
   const inputs = form.querySelectorAll("input, textarea");
 
   form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
     form.querySelectorAll(".error-message").forEach(function (errorDiv) {
       errorDiv.remove();
     });
 
     let allInputsValid = true;
+
     inputs.forEach(function (input) {
       let errorMessage = "";
 
@@ -15,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (input.name === "name") {
         if (!checkWordLength(input.value, 5)) {
           errorMessage = "Name must be at least 5 characters";
-          allValid = false;
+          allInputsValid = false;
         }
       }
 
@@ -23,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (input.name === "email") {
         if (!validateEmail(input.value)) {
           errorMessage = "Please enter a valid email address";
-          allValid = false;
+          allInputsValid = false;
         }
       }
 
@@ -31,7 +36,15 @@ document.addEventListener("DOMContentLoaded", function () {
       if (input.name === "subject") {
         if (!checkWordLength(input.value, 15)) {
           errorMessage = "Subject must be at least 15 characters";
-          allValid = false;
+          allInputsValid = false;
+        }
+      }
+
+      // message: should be more than 25 characters
+      if (input.name === "message") {
+        if (!checkWordLength(input.value, 25)) {
+          errorMessage = "Message must be at least 25 characters";
+          allInputsValid = false;
         }
       }
 
@@ -47,9 +60,26 @@ document.addEventListener("DOMContentLoaded", function () {
     // Prevent form submission if there are invalid inputs
     if (!allInputsValid) {
       event.preventDefault();
+      displayMessage("Invalid submission. Please correct and try again.");
+      removeMessage();
+    } else {
+      form.reset();
+      displayMessage("Form submitted successfully!");
+      removeMessage();
     }
   });
 });
+
+function displayMessage(message) {
+  submitMessage.style.display = "block";
+  submitMessage.innerHTML = message;
+}
+
+function removeMessage() {
+  setTimeout(function () {
+    submitMessage.style.display = "none";
+  }, 3000);
+}
 
 function validateEmail(email) {
   const regEx = /\S+@\S+\.\S+/;
@@ -63,14 +93,4 @@ function checkWordLength(input, requiredLength) {
   } else {
     return false;
   }
-}
-
-function clearFormData() {
-  nameInput.value = "";
-  emailInput.value = "";
-  subjectInput.value = "";
-  addressInput.value = "";
-  // setTimeout(function(){
-  //     succMessage.style.display = "none";
-  // }, 5000)
 }
