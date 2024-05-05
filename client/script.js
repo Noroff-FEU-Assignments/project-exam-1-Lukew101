121e5f9a09628b10afc6b54297043e591d3000d3
Luke <lukewilliams10101@gmail.com>
2024-05-05 16:28:10 +0200

    feat(): overlay added

--- client/script.js
+++ client/script.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector(".burger-menu");
 const nav = document.querySelector(".header-nav");
+const overlay = document.querySelector(".overlay");
 
 // Nav menu
 const navSlide = () => {
@@ -7,10 +8,25 @@ const navSlide = () => {
     if (nav.classList.contains("nav-active")) {
       nav.classList.remove("nav-active");
       burger.classList.remove("toggle");
+      overlay.classList.remove("overlay-opn");
     } else {
       nav.classList.add("nav-active");
       burger.classList.add("toggle");
+      overlay.classList.add("overlay-opn");
     }
   });
 };
 navSlide();
+
+function closeNavFromOutside() {
+  document.addEventListener("click", function (e) {
+    if (!e.target.closest("nav") && e.target != burger) {
+      if (nav.classList.contains("nav-active")) {
+        nav.classList.remove("nav-active");
+        burger.classList.remove("toggle");
+        overlay.classList.remove("overlay-opn");
+      }
+    }
+  });
+}
+closeNavFromOutside();
